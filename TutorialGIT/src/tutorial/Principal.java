aa18416ebd9375a6c129de7aafc83488573127e8
Hoscanoa <hoscanoa@gmail.com>
2014-04-14 23:42:25 -0500

    Update Principal.java

--- TutorialGIT/src/tutorial/Principal.java
+++ TutorialGIT/src/tutorial/Principal.java
@@ -7,7 +7,10 @@ public class Principal {
 	 */
 	public static void main(String[] args) {
 		try {
-			System.out.println("Mensaje");
+			System.out.println("Mensaje1");
+			System.out.println("Mensaje2");
+			System.out.println("Mensaje3");
+			System.out.println("Mensaje4");
 		} catch (NullPointerException e) {
 			System.out.println("Controlando punteros nulos");
 		} catch (Exception e) {
