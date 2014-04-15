a0322cb46f3b873d295f5ef27f471e3fb844b609
hoscanoa <hoscanoa@gmail.com>
2014-04-14 23:39:40 -0500

    NullPointerException

--- TutorialGIT/src/tutorial/Principal.java
+++ TutorialGIT/src/tutorial/Principal.java
@@ -8,10 +8,11 @@ public class Principal {
 	public static void main(String[] args) {
 		try {
 			System.out.println("Mensaje");
+		} catch (NullPointerException e) {
+			System.out.println("Controlando punteros nulos");
 		} catch (Exception e) {
 			System.out.println("Esta todo controlado");
 		}
 
 	}
-
 }
